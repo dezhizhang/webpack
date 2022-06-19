0d54180a063266d9cc1d6f1f754c10559e7c30fc
dezhizhang <1541609448@qq.com>
2022-06-19 09:22:52 +0800

    feat:编译

--- lab/Compilation.js
+++ lab/Compilation.js
@@ -1,5 +1,40 @@
+const { Tapable, SyncHook } = require('tapable');
+const NormalModuleFactory = require('./NormalModuleFactory');
 
-class Compilation{
+const normalModuleFactory = new NormalModuleFactory();
+
+
+class Compilation extends Tapable{
+    constructor(compiler) {
+        super();
+        this.compiler = compiler;
+        this.options = compiler.options;
+        this.context = compiler.context;
+        this.inputFileSystem = compiler.inputFileSystem;
+        this.outputFileSystem = compiler.outputFileSystem;
+        this.enteries = [];
+        this.modules = [];
+        this.hooks = {
+            succeedModule:new SyncHook(['module'])
+        }
+    }
+
+    addEntry(context,entry,name,callback) {
+        this._addModuleChain(context,entry,name,(err,module) => {
+            callback(err,module);
+        })
+    }
+
+    _addModuleChain(context,rawRequest,name,callback) {
+       let entryModule =  normalModuleFactory.create({
+            name,
+            context,
+            rawRequest,
+            resource:path.posix.join(context,entry)
+        });
+        this.enteries.push(entryModule);
+        this.modules.push(entryModule);
+    }
 
 }
 
