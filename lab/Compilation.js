563e0ea96deff17c6baf1ca1df1c31cffbe035f3
dezhizhang <1541609448@qq.com>
2022-06-19 11:15:43 +0800

    feat:编译模块

--- lab/Compilation.js
+++ lab/Compilation.js
@@ -6,7 +6,7 @@ const parser = new Parser();
 const normalModuleFactory = new NormalModuleFactory();
 
 
-class Compilation extends Tapable{
+class Compilation extends Tapable {
     constructor(compiler) {
         super();
         this.compiler = compiler;
@@ -17,39 +17,50 @@ class Compilation extends Tapable{
         this.enteries = [];
         this.modules = [];
         this.hooks = {
-            succeedModule:new SyncHook(['module'])
+            succeedModule: new SyncHook(['module'])
         }
     }
 
-    addEntry(context,entry,name,callback) {
-        this._addModuleChain(context,entry,name,(err,module) => {
-            callback(err,module);
+    addEntry(context, entry, name, callback) {
+        this._addModuleChain(context, entry, name, (err, module) => {
+            callback(err, module);
         })
     }
 
-    _addModuleChain(context,rawRequest,name,callback) {
-       let entryModule =  normalModuleFactory.create({
+    _addModuleChain(context, rawRequest, name, callback) {
+        let entryModule = normalModuleFactory.create({
             name,
             context,
             rawRequest,
-            resource:path.posix.join(context,entry),
+            resource: path.posix.join(context, entry),
             parser
         });
         this.enteries.push(entryModule);
         this.modules.push(entryModule);
 
-        const afterBuild = (err) => {
-            return callback(err,module);
+        const afterBuild = (err, module) => {
+            if (module.dependencies.length > 0) {
+                this.processModuleDependencies(module, err => {
+                    callback(err, module);
+                })
+            }else {
+                callback(module);
+            }
         }
 
-        this.buildModule(entryModule,afterBuild);
+        this.buildModule(entryModule, afterBuild);
     }
-    
-    buildModule(module,afterBuild) {
-        module.build(this,(err) => {
+
+    buildModule(module, afterBuild) {
+        module.build(this, (err) => {
             this.hooks.succeedModule.call(module);
             afterBuild(err);
         })
+    }
+    processModuleDependencies(module,callback) {
+        //获取当前模块的依赖模块
+        let dependencies = module.dependencies;
+
     }
 }
 
--- lab/NormalModule.js
+++ lab/NormalModule.js
@@ -1,4 +1,8 @@
 
+const path = require('path');
+const types = require('babel-types');
+const generate = require('babel-generator').default;
+const traverse = require('babel-traverse').default;
 
 class NormalModule{
     constructor({name,context,rawRequest,resource,parser}) {
@@ -9,11 +13,34 @@ class NormalModule{
         this.parser = parser;
         this._source;
         this._ast;
+        this.dependencies = [];
     }
     build(compilation,callback) {
         this.doBuild(compilation,err => {
             this._ast = this.parser.parse(this._source);
-            callback();
+            traverse(this._ast,{
+                CallExpression:(nodePath) => {
+                    let node = nodePath.node;
+                    if(node.callee.name === 'require') {
+                        node.callee.name = '__webpack_require__';
+                        let moduleName = node.arguments[0].value;
+                        let extName = moduleName.split(path.posix.sep).pop().indexOf('.') === -1 ? '.js':'';
+                        let depResource = path.posix.join(path.posix.dirname(this.resource),this.moduleName + extName);
+                        let depModuleId = './' + path.posix.relative(this.context,depResource);
+                        node.arguments = [types.stringLiteral(depModuleId)];
+                        this.dependencies.push({
+                            name:this.name,
+                            context:this.context,
+                            rawRequest:moduleName,
+                            moduleId:depModuleId,
+                            resource:depResource
+                        });
+                            
+
+                    }
+                }
+            })
+            // callback();
         })
     }
 
